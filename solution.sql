8bce7c21b534b2238a04d23b5f16a020b77409be
SiYeonHan <itsiyeon@nate.com>
2021-05-14 17:18:58 +0900

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -53,3 +53,13 @@ ORDER BY HOUR ASC;
 -- 역순 정렬하기
 SELECT name, datetime from animal_ins order by animal_id desc;                      
                       
+-- 동물의 아이디와 이름
+SELECT animal_id, name from animal_ins order by animal_id;
+
+--  여러가지 기준으로 정렬하기
+SELECT animal_id, name, datetime from animal_ins order by name, datetime desc
+                      
+--  상위 n개 레코드 
+SELECT name from animal_ins where datetime = (select min(datetime) from animal_ins)
+                    
+                      
